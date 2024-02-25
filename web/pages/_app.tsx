547e5cdba4ca79dc9ce40e27f0687323f1aa6e47
latioms <latioms@gmail.com>
2024-02-25 10:51:41 +0100

    #6: -Ajouter _app.tsx, favicon.ico et globals.css - renommer app en pages  pour changer le systeme de routage

---
+++ web/pages/_app.tsx
@@ -0,0 +1,16 @@
+import type {AppProps} from 'next/app'
+import {ThemeContextProvider} from "@/pages/helper/ThemeContextProvider";
+import {ApplicationConfigs} from "@/pages/hooks/useApplicationConfigs";
+
+function App({Component, pageProps}: AppProps) {
+
+  return (
+    <ApplicationConfigs>
+      <ThemeContextProvider>
+        <Component {...pageProps} />
+      </ThemeContextProvider>
+    </ApplicationConfigs>
+  )
+}
+
+export default App
