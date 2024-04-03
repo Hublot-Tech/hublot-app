9845b4533beae647fb7ea82a73591bc8e072096b
germann <germannpessidjo6@gmail.com>
2024-04-03 11:45:07 +0100

    modification de search_box

--- app/lib/screens/part_customer/home_screens/components/search_box.dart
+++ app/lib/screens/part_customer/home_screens/components/search_box.dart
@@ -8,24 +8,27 @@ class SearchBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      canRequestFocus: false,
-      onTap: () {
-        Navigator.pushNamed(context, SearchScreen.routeName);
-      },
-      decoration: InputDecoration(
-        prefixIcon: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.search,
-              size: 30,
-            )),
-        contentPadding: const EdgeInsets.only(left: 30),
-        label: const Text("Que recherchez vous?"),
-        hintText: 'ed',
-        border: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(30),
+    return Padding(
+      padding: const EdgeInsets.only(right: 20),
+      child: TextField(
+        canRequestFocus: false,
+        onTap: () {
+          Navigator.pushNamed(context, SearchScreen.routeName);
+        },
+        decoration: InputDecoration(
+          prefixIcon: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                size: 30,
+              )),
+          contentPadding: const EdgeInsets.only(left: 30),
+          label: const Text("Que recherchez vous?"),
+          hintText: 'ed',
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(30),
+            ),
           ),
         ),
       ),
