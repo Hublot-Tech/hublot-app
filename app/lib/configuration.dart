4bd3bda9efd766da4cc5f02c4ea844a9ceef6056
germann <germannpessidjo6@gmail.com>
2024-05-01 19:35:31 +0100

    ajout des configurations

--- app/lib/configuration.dart
+++ app/lib/configuration.dart
@@ -6,6 +6,10 @@ const kyellowColor = Color.fromRGBO(255, 192, 0, 1);
 const kprimaryColor = Color.fromRGBO(10, 17, 40, 1);
 const ksecondaryColor = Color.fromRGBO(100, 100, 100, 1);
 const kbottonColor = Color.fromRGBO(100, 100, 100, 40);
+const ktransparentColor = Color.fromRGBO(217, 217, 217, 1);
+const kgreenColor = Color.fromRGBO(51, 196, 129, 1);
+const kredColor = Color.fromRGBO(229, 57, 53, 1);
+const kcolorBoxService = Color.fromRGBO(10, 17, 40, 0.1);
 Text textPresentation(
     {required String msg,
     double size = 30,
