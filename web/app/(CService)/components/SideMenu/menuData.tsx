4fb5890ae9940d54a4929a8e4c993e9623e1e3fd
latioms <latioms@gmail.com>
2024-05-02 11:43:42 +0100

    Refactorisation du composant SideMenu et ajout des données de menu

---
+++ web/app/(CService)/components/SideMenu/menuData.tsx
@@ -0,0 +1,46 @@
+import { Menu } from "@/types/menu";
+
+export const menuData: Menu[] = [
+    {
+        id: 1,
+        title: "Dashboard",
+        icon: "dashboard",
+        path: "/Dashboard",
+        newTab: false,
+    },
+    {
+        id: 2,
+        title: "Customers",
+        icon: "people",
+        path: "/Customers",
+        newTab: false,
+    },
+    {
+        id: 3,
+        title: "Chat",
+        icon: "chat",
+        path: "/chat",
+        newTab: false,
+    },
+    {
+        id: 4,
+        title: "Email",
+        icon: "email",
+        path: "/email",
+        newTab: false,
+    },
+    {
+        id: 5,
+        title: "Settings",
+        icon: "settings",
+        path: "/settings",
+        newTab: false,
+    },
+    {
+        id: 6,
+        title: "Help",
+        icon: "help",
+        path: "/help",
+        newTab: false,
+    },
+];
