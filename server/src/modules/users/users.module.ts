b15ad54bb32ee4afa138cd82aa2088763b079632
latioms <latioms@gmail.com>
2024-05-10 09:04:08 +0100

    feat: Add new modules for chat, order, payment, service, client, and provider

--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -6,10 +6,14 @@ import { AuthGuard } from "../authentication/auth.guard";
 import { User, UserSchema } from "./schema/users.schema";
 import { UsersController } from "./users.contoller";
 import { UserService } from "./users.service";
+import { ClientModule } from './client/client.module';
+import { ProvModule } from './prov/prov.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ClientModule,
+    ProvModule,
   ],
   controllers: [UsersController],
   providers: [
