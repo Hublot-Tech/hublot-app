5941489204416dcc8a4c7d0f0508b13bc8e02574
latioms <latioms@gmail.com>
2024-05-10 20:12:34 +0100

    chore: Add chat module with controller and service

--- server/src/modules/chat/chat.module.ts
+++ server/src/modules/chat/chat.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
 
-@Module({})
+@Module({
+  providers: [ChatService],
+  controllers: [ChatController]
+})
 export class ChatModule {}
