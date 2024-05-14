d8eedee8f30d922f42428f976173ed022e44c2a6
germann <germannpessidjo6@gmail.com>
2024-05-14 17:02:56 +0100

    refacto(blot):refacto code

---
+++ app/lib/controller/blot.dart
@@ -0,0 +1,10 @@
+class Blot {
+  String? idBlot;
+  String? description;
+  int? price;
+  DateTime? beginDate;
+  DateTime? endDate;
+  Blot(this.idBlot, this.description, this.price, this.beginDate, this.endDate);
+}
+
+List<Blot> listBlot = [];
--- app/lib/screens/part_provider/blot_screen/components/body.dart
+++ app/lib/screens/part_provider/blot_screen/components/body.dart
@@ -2,11 +2,16 @@ import 'package:app/configuration.dart';
 import 'package:app/screens/authentification/registration_screen/component/button_custom.dart';
 import 'package:app/screens/part_provider/add_service/description_service/components/background_add_service.dart';
 import 'package:app/screens/part_provider/add_service/service_offer/components/add_service_box.dart';
+import 'package:app/screens/part_provider/blot_preview_screen/blot_preview_screen.dart';
+import 'package:app/screens/part_provider/blot_preview_screen/component/decoration_box_widget.dart';
 import 'package:app/size_configuration.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:app/controller/blot.dart';
 
 import '../../add_service/service_offer/components/cross_widget.dart';
+import 'box_date_blots.dart';
+import 'cancel_button.dart';
 
 class Body extends StatefulWidget {
   const Body({super.key});
@@ -31,6 +36,12 @@ class _BodyState extends State<Body> {
     });
   }
 
+  final descriptionController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  final jourController = TextEditingController();
+  final moisController = TextEditingController();
+  final anneeController = TextEditingController();
+  bool isComplete = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -39,241 +50,246 @@ class _BodyState extends State<Body> {
         padding: EdgeInsets.only(
             left: getProportionateScreenWidth(20),
             right: getProportionateScreenWidth(20)),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 05, top: 25),
-              child: CrossWidgetClick(
-                routeName: 'DescriptionService.routeName',
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 05, top: 25),
+                child: CrossWidgetClick(
+                  routeName: 'DescriptionService.routeName',
+                ),
               ),
-            ),
-            const EspaceMenuWidget(taille: 70),
-            textPresentation(
-                msg: "Création du Blot #0001",
-                fontWeight: FontWeight.normal,
-                color: kprimaryColor,
-                size: 24.12),
-            Padding(
-              padding: EdgeInsets.only(
-                  left: getProportionateScreenWidth(57),
-                  right: getProportionateScreenWidth(57)),
-              child: textPresentation(
+              const EspaceMenuWidget(taille: 70),
+              textPresentation(
+                  msg: "Création du Blot #0001",
+                  fontWeight: FontWeight.bold,
                   color: kprimaryColor,
-                  msg:
-                      "Toutes informations inserer servira en cas de litige et aussi faciliter l’échange",
-                  fontWeight: FontWeight.normal,
-                  size: 11),
-            ),
-            const EspaceMenuWidget(taille: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    textPresentation(
-                        color: kprimaryColor,
-                        msg: "Offre de base",
-                        fontWeight: FontWeight.bold,
-                        size: 24.12),
-                    Row(
-                      children: [
-                        textPresentation(
-                            color: kprimaryColor,
-                            msg: "Sélectionner un offre",
-                            fontWeight: FontWeight.normal,
-                            size: 13),
-                        const Icon(Icons.keyboard_arrow_down_outlined),
-                      ],
-                    )
-                  ],
-                ),
-                Container(
-                  width: getProportionateScreenWidth(115),
-                  height: getProportionateScreenHeight(32),
-                  decoration: BoxDecoration(
-                      color: kyellowColor,
-                      borderRadius: BorderRadius.circular(5)),
-                  child: Center(
-                    child: textPresentation(
-                        msg: "10 000 FCFA",
-                        fontWeight: FontWeight.bold,
-                        color: kprimaryColor,
-                        size: 15),
+                  size: 24.12),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: getProportionateScreenWidth(57),
+                    right: getProportionateScreenWidth(57)),
+                child: textPresentation(
+                    color: kprimaryColor,
+                    msg:
+                        "Toutes informations inserer servira en cas de litige et aussi faciliter l’échange",
+                    fontWeight: FontWeight.normal,
+                    size: 11),
+              ),
+              const EspaceMenuWidget(taille: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      textPresentation(
+                          color: kprimaryColor,
+                          msg: "Offre de base",
+                          fontWeight: FontWeight.bold,
+                          size: 24.12),
+                      Row(
+                        children: [
+                          textPresentation(
+                              color: kprimaryColor,
+                              msg: "Sélectionner un offre",
+                              fontWeight: FontWeight.normal,
+                              size: 13),
+                          const Icon(Icons.keyboard_arrow_down_outlined),
+                        ],
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-            const EspaceMenuWidget(taille: 25),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                textPresentation(
-                    msg: "Description",
-                    fontWeight: FontWeight.bold,
-                    size: 24.12,
-                    color: kprimaryColor),
-                Row(
-                  children: [
-                    textPresentation(
-                        msg: "Modifier",
-                        fontWeight: FontWeight.normal,
-                        size: 13),
-                    SvgPicture.asset("img/icons8_edit_2 1.svg"),
-                  ],
-                ),
-              ],
-            ),
-            const EspaceMenuWidget(),
-            TextField(
-              maxLines: 12,
-              decoration: InputDecoration(
-                hintStyle: const TextStyle(
-                    fontSize: 13, fontWeight: FontWeight.normal),
-                hintText:
-                    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonu Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonu Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonu...",
-                contentPadding: const EdgeInsets.only(left: 30, top: 32),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+                  DecorationBoxWidget(
+                      width: 115,
+                      height: 32,
+                      color: kyellowColor,
+                      havePadding: false,
+                      args: Center(
+                        child: textPresentation(
+                            msg: "10 000 FCFA",
+                            fontWeight: FontWeight.bold,
+                            color: kprimaryColor,
+                            size: 15),
+                      )),
+                ],
+              ),
+              const EspaceMenuWidget(taille: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  textPresentation(
+                      msg: "Description",
+                      fontWeight: FontWeight.bold,
+                      size: 24.12,
+                      color: kprimaryColor),
+                  Row(
+                    children: [
+                      textPresentation(
+                          msg: "Modifier",
+                          fontWeight: FontWeight.normal,
+                          size: 13),
+                      SvgPicture.asset("img/icons8_edit_2 1.svg"),
+                    ],
+                  ),
+                ],
+              ),
+              const EspaceMenuWidget(),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter someting';
+                  } else {
+                    descriptionController.text = value;
+                  }
+                },
+                onChanged: (value) {
+                  setState(() {
+                    isComplete = !isComplete;
+                  });
+                },
+                maxLines: 12,
+                decoration: InputDecoration(
+                  hintStyle: const TextStyle(
+                      fontSize: 13, fontWeight: FontWeight.normal),
+                  hintText:
+                      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonu Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonu Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonu...",
+                  contentPadding: const EdgeInsets.only(left: 30, top: 32),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
               ),
-            ),
-            const EspaceMenuWidget(),
-            Row(
-              children: [
-                textPresentation(
-                    msg: "Sélectionner des options",
-                    fontWeight: FontWeight.bold,
-                    size: 24.12,
-                    color: kprimaryColor),
-              ],
-            ),
-            const EspaceMenuWidget(),
-            Container(
-              height: getProportionateScreenHeight(50),
-              width: getProportionateScreenWidth(290),
-              decoration: const BoxDecoration(
-                  border: Border(
-                top: BorderSide(),
-                bottom: BorderSide(),
-              )),
-              child: AddServiceBox(
-                msg: "Ajouter des options ",
-                press: () {},
+              const EspaceMenuWidget(),
+              Row(
+                children: [
+                  textPresentation(
+                      msg: "Sélectionner des options",
+                      fontWeight: FontWeight.bold,
+                      size: 24.12,
+                      color: kprimaryColor),
+                ],
               ),
-            ),
-            const EspaceMenuWidget(),
-            Row(
-              children: [
-                textPresentation(
-                    msg: "Date de début",
-                    fontWeight: FontWeight.bold,
-                    size: 24.12,
-                    color: kprimaryColor),
-              ],
-            ),
-            const EspaceMenuWidget(),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                BoxDateBlots(width: 50),
-                BoxDateBlots(width: 50),
-                BoxDateBlots(width: 64),
-              ],
-            ),
-            const EspaceMenuWidget(),
-            Row(
-              children: [
-                textPresentation(
-                    msg: "Durée du blot",
-                    fontWeight: FontWeight.bold,
-                    size: 24.12,
-                    color: kprimaryColor),
-              ],
-            ),
-            const EspaceMenuWidget(),
-            Row(children: [
+              const EspaceMenuWidget(),
               Container(
                 height: getProportionateScreenHeight(50),
-                width: getProportionateScreenWidth(58),
-                margin: EdgeInsets.only(left: getProportionateScreenWidth(85)),
+                width: getProportionateScreenWidth(290),
                 decoration: const BoxDecoration(
                     border: Border(
                   top: BorderSide(),
                   bottom: BorderSide(),
                 )),
-                child: Row(
-                  children: [
-                    const Spacer(flex: 2),
-                    textPresentation(
-                        msg: "JJ", fontWeight: FontWeight.normal, size: 13),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.keyboard_arrow_down_outlined)),
-                  ],
+                child: AddServiceBox(
+                  msg: "Ajouter des options ",
+                  press: () {},
                 ),
               ),
-              SizedBox(width: getProportionateScreenWidth(30)),
-              IconButton(onPressed: onAdd, icon: const Icon(Icons.add)),
-              textPresentation(msg: '$duree', fontWeight: FontWeight.normal),
-              IconButton(onPressed: onRemove, icon: const Icon(Icons.remove)),
-            ]),
-            const EspaceMenuWidget(taille: 30),
-            const Divider(
-              indent: 90,
-              endIndent: 90,
-              thickness: 4,
-              color: Colors.black,
-            ),
-            const EspaceMenuWidget(),
-            ButtomCustom(press: () {}, msg: "Aperçu", isValided: false),
-            const EspaceMenuWidget(),
-            Container(
-              height: getProportionateScreenHeight(50),
-              width: getProportionateScreenWidth(390),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 2),
-                borderRadius: BorderRadius.circular(7),
-                // color: ,
+              const EspaceMenuWidget(),
+              Row(
+                children: [
+                  textPresentation(
+                      msg: "Date de début",
+                      fontWeight: FontWeight.bold,
+                      size: 24.12,
+                      color: kprimaryColor),
+                ],
               ),
-              child: Center(
-                child: textPresentation(
-                    msg: "Annuler",
-                    fontWeight: FontWeight.normal,
-                    size: 20,
-                    color: Colors.black),
+              const EspaceMenuWidget(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  BoxDateBlots(
+                      width: 50, controller: jourController, hint: "JJ"),
+                  BoxDateBlots(
+                    width: 50,
+                    controller: moisController,
+                    hint: "MM",
+                  ),
+                  BoxDateBlots(
+                    width: 64,
+                    controller: anneeController,
+                    hint: "ANNEE",
+                  ),
+                ],
               ),
-            ),
-            EspaceMenuWidget(taille: 50),
-          ],
+              const EspaceMenuWidget(),
+              Row(
+                children: [
+                  textPresentation(
+                      msg: "Durée du blot",
+                      fontWeight: FontWeight.bold,
+                      size: 24.12,
+                      color: kprimaryColor),
+                ],
+              ),
+              const EspaceMenuWidget(),
+              Row(children: [
+                Container(
+                  height: getProportionateScreenHeight(50),
+                  width: getProportionateScreenWidth(58),
+                  margin:
+                      EdgeInsets.only(left: getProportionateScreenWidth(85)),
+                  decoration: const BoxDecoration(
+                      border: Border(
+                    top: BorderSide(),
+                    bottom: BorderSide(),
+                  )),
+                  child: Row(
+                    children: [
+                      const Spacer(flex: 2),
+                      textPresentation(
+                          msg: "$duree",
+                          fontWeight: FontWeight.normal,
+                          size: 13),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.keyboard_arrow_down_outlined)),
+                    ],
+                  ),
+                ),
+                SizedBox(width: getProportionateScreenWidth(30)),
+                IconButton(onPressed: onAdd, icon: const Icon(Icons.add)),
+                textPresentation(msg: '$duree', fontWeight: FontWeight.normal),
+                IconButton(onPressed: onRemove, icon: const Icon(Icons.remove)),
+              ]),
+              const EspaceMenuWidget(taille: 30),
+              const Divider(
+                indent: 90,
+                endIndent: 90,
+                thickness: 4,
+                color: Colors.black,
+              ),
+              const EspaceMenuWidget(),
+              ButtomCustom(
+                  press: () {
+                    debugPrint(listBlot.length.toString());
+                    int anneeInt = int.parse(2003.toString());
+                    int moisInt = int.parse(04.toString());
+                    int jourInt = int.parse(03.toString());
+                    if (formKey.currentState!.validate()) {
+                      DateTime beginDate = DateTime(anneeInt, moisInt, jourInt);
+                      int jourEnd = jourInt + duree;
+
+                      DateTime endDate = DateTime(anneeInt, moisInt, jourEnd);
+                      Blot blot = Blot('0001', descriptionController.text,
+                          10000, beginDate, endDate);
+                      listBlot.add(blot);
+                      debugPrint(listBlot.length.toString());
+                      Navigator.pushNamed(context, BlotPreviewScrenn.routeName,
+                          arguments: blot);
+                    }
+                  },
+                  msg: "Aperçu",
+                  isValided: isComplete),
+              const EspaceMenuWidget(),
+              CancelButtom(
+                press: () {},
+              ),
+              const EspaceMenuWidget(taille: 50),
+            ],
+          ),
         ),
       )),
-    );
-  }
-}
-
-class BoxDateBlots extends StatelessWidget {
-  const BoxDateBlots({
-    super.key,
-    required this.width,
-  });
-  final double width;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: getProportionateScreenHeight(50),
-      width: getProportionateScreenWidth(width),
-      decoration: const BoxDecoration(
-          border: Border(
-        top: BorderSide(),
-        //bottom: BorderSide(),
-      )),
-      child: TextField(
-        keyboardType: TextInputType.number,
-        decoration: InputDecoration(
-            hintText: "JJ",
-            contentPadding:
-                EdgeInsets.only(left: getProportionateScreenWidth(14))),
-      ),
     );
   }
 }
