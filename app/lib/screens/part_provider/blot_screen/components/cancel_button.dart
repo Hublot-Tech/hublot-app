ebe7641daccd35cb6b491a61bd28fa57dc34916e
germann <germannpessidjo6@gmail.com>
2024-05-27 21:09:38 +0100

    fix(authentification):ajustement

--- app/lib/screens/part_provider/blot_screen/components/cancel_button.dart
+++ app/lib/screens/part_provider/blot_screen/components/cancel_button.dart
@@ -4,16 +4,18 @@ import 'package:flutter/material.dart';
 
 class CancelButtom extends StatelessWidget {
   const CancelButtom({
-    super.key, required this.press,
+    super.key,
+    required this.press,
   });
   final GestureCancelCallback press;
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: press,
       child: Container(
         height: getProportionateScreenHeight(50),
-        width: getProportionateScreenWidth(390),
+        width: size.width * 0.9,
         decoration: BoxDecoration(
           border: Border.all(color: Colors.black, width: 2),
           borderRadius: BorderRadius.circular(7),
